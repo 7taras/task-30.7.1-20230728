8d2e17773c2588af432d39f06f558127a9034a12
7taras <7taras@mail.ru>
2023-08-06 23:42:33 +0300

    ...

---
+++ task-30.7.1-20230728/RequestHandler.cpp
@@ -0,0 +1,16 @@
+#include "RequestHandler.h"
+
+RequestHandler::RequestHandler() 
+{
+	m_tpool.start();
+}
+
+RequestHandler::~RequestHandler() 
+{
+	m_tpool.stop();
+}
+
+void RequestHandler::pushRequest(FuncType f, int id, int arg) 
+{
+	m_tpool.push_task(f, id, arg);
+}
---
+++ task-30.7.1-20230728/RequestHandler.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "ThreadPool.h"
+
+class RequestHandler 
+{
+public:
+    RequestHandler();
+    ~RequestHandler();
+    // отправка запроса на выполнение
+    void pushRequest(FuncType f, int id, int arg);
+private:
+    // пул потоков
+    ThreadPool m_tpool;
+};
+
